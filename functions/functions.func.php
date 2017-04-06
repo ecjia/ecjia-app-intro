b7706c8995f4ba15a462f820e72d611177e251f8
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-06 11:50:34 +0800

    分离出intro应用

---
+++ functions/functions.func.php
@@ -0,0 +1,13 @@
+<?php
+
+const APP_STORAGEKEY_TEMPLATE = 'app_template';
+const APP_STORAGEKEY_SYTLENAME = 'app_stylename';
+
+RC_Hook::add_filter('ecjia_theme_template_code', function() {
+    return APP_STORAGEKEY_TEMPLATE;
+});
+
+RC_Hook::add_filter('ecjia_theme_stylename_code', function() {
+    return APP_STORAGEKEY_SYTLENAME;
+});
+
