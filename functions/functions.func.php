586388aefd424c09b9bf04506ee3162e339067c9
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-06 15:49:26 +0800

    调试

--- functions/functions.func.php
+++ functions/functions.func.php
@@ -3,11 +3,11 @@
 const APP_STORAGEKEY_TEMPLATE = 'app_template';
 const APP_STORAGEKEY_SYTLENAME = 'app_stylename';
 
-RC_Hook::add_filter('ecjia_theme_template_code', function() {
+RC_Hook::add_filter('ecjia_theme_template_code', function($code) {
     return APP_STORAGEKEY_TEMPLATE;
 });
 
-RC_Hook::add_filter('ecjia_theme_stylename_code', function() {
+RC_Hook::add_filter('ecjia_theme_stylename_code', function($code) {
     return APP_STORAGEKEY_SYTLENAME;
 });
 
