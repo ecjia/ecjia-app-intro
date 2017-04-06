788b7a44493d08ec032d2be91abebb33f4af9a0e
royalwang <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-06 20:38:56 +0800

    调试完毕，恢复代码

--- functions/functions.func.php
+++ functions/functions.func.php
@@ -1,13 +1,13 @@
 <?php
 
-// const APP_STORAGEKEY_TEMPLATE = 'app_template';
-// const APP_STORAGEKEY_SYTLENAME = 'app_stylename';
+const APP_STORAGEKEY_TEMPLATE = 'app_template';
+const APP_STORAGEKEY_SYTLENAME = 'app_stylename';
 
 RC_Hook::add_filter('ecjia_theme_template_code', function($code) {
-    return 'app_template';
+    return APP_STORAGEKEY_TEMPLATE;
 });
 
 RC_Hook::add_filter('ecjia_theme_stylename_code', function($code) {
-    return 'app_stylename';
+    return APP_STORAGEKEY_SYTLENAME;
 });
 
