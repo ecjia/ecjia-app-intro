d0a1593dd2ae9c74e5e4f8f75ddc210a52bf5243
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-06 15:41:47 +0800

    删除多余方法

--- index.php
+++ index.php
@@ -122,46 +122,8 @@ class index extends ecjia_front {
 			$this->assign('shop_logo', 				$shop_logo);
 			$this->assign('shop_wechat_qrcode', 	$shop_wechat_qrcode);
 			$this->assign('powered', 	'Powered&nbsp;by&nbsp;<a href="https:\/\/ecjia.com" target="_blank">ECJia</a>');
-			
-        	$categoryGoods = array();
-			RC_Loader::load_app_class('goods_category', 'goods', false);
-			$category = goods_category::get_categories_tree();
-			$category = array_merge($category);
-			
-			if (!empty($category)) {
-				foreach($category as $key => $val) {
-					$categoryGoods[$key]['id'] = $val['id'];
-					$categoryGoods[$key]['name'] = $val['name'];
-					$categoryGoods[$key]['image'] = $val['img'];
-					if (!empty($val['cat_id'])) {
-						foreach($val['cat_id'] as $k => $v ) {
-							$categoryGoods[$key]['children'][$k] = array(
-								'id'     => $v['id'],
-								'name'   => $v['name'],
-								'image'	 => $v['img'],
-							);
-								
-							if( !empty($v['cat_id']) ) {
-								foreach($v['cat_id'] as $k1 => $v1) {
-									$categoryGoods[$key]['children'][$k]['children'][] = array(
-										'id'     => $v1['id'],
-										'name'   => $v1['name'],
-										'image'	 => $v1['img'],
-									);
-								}
-							} else {
-								$categoryGoods[$key]['children'][$k]['children'] = array();
-							}
-								
-							$categoryGoods[$key]['children'] = array_merge($categoryGoods[$key]['children']);
-						}
-					} else {
-						$categoryGoods[$key]['children'] = array();
-					}
-				}
-			}
-			$this->assign('cat_list', $categoryGoods);
         }
+        
         $this->display('index.dwt', $cache_id);
     }
 }
